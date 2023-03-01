beb40c0b0b08649e84c0fa3195de2988788795ef
giang <nhgiang9992@gmail.com>
2023-03-01 20:23:28 +1030

    create client side

--- API/Controllers/ProductController.cs
+++ API/Controllers/ProductController.cs
@@ -19,14 +19,14 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await context.Products.ToListAsync();
+            var products = await _context.Products.ToListAsync();
             return Ok(products);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await context.Products.FindAsync(id);
+            return await _context.Products.FindAsync(id);
         }
     }
 }
